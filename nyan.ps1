1b2e28b80767f0fc414805aac334d0dd4428e1e0
Roario0602 <153424711+Roario0602@users.noreply.github.com>
2025-07-27 15:27:37 +0600

    Update nyan.ps1

--- nyan.ps1
+++ nyan.ps1
@@ -1,16 +1,8 @@
+# Максимизировать окно PowerShell (новое окно)
 $script = @'
 Clear-Host
-Write-Host "
-██╗   ██╗ ██████╗ ██╗   ██╗    ██████╗  ██████╗ ██████╗ ███████╗
-██║   ██║██╔═══██╗██║   ██║    ██╔══██╗██╔═══██╗██╔══██╗██╔════╝
-██║   ██║██║   ██║██║   ██║    ██████╔╝██║   ██║██████╔╝█████╗  
-╚██╗ ██╔╝██║   ██║██║   ██║    ██╔═══╝ ██║   ██║██╔═══╝ ██╔══╝  
- ╚████╔╝ ╚██████╔╝╚██████╔╝    ██║     ╚██████╔╝██║     ███████╗
-  ╚═══╝   ╚═════╝  ╚═════╝     ╚═╝      ╚═════╝ ╚═╝     ╚══════╝
-" -ForegroundColor Red
-
-Write-Host "YOUR PC DESTROYED! 💥" -ForegroundColor DarkRed
-Read-Host "Введите код, чтобы попытаться восстановить систему"
+Write-Host "YOUR PC DESTROYED!" -ForegroundColor Red
+Read-Host "Введите код для разблокировки"
 '@
 
 Start-Process powershell -ArgumentList "-NoExit", "-Command", $script -WindowStyle Maximized
